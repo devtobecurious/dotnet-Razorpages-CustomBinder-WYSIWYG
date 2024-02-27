eeaf608cb2ff607ecdf58f73110c99cab4cc3f5e
Evan BOISSONNOT <evan@boissonnot.fr>
2024-02-27 19:18:09 +0100

    feat: upload image is ok

--- CustomBinderAndWYSIWYG.Web.UI/Pages/TestTinyMCE.cshtml.cs
+++ CustomBinderAndWYSIWYG.Web.UI/Pages/TestTinyMCE.cshtml.cs
@@ -1,16 +1,48 @@
 using CustomBinderAndWYSIWYG.Web.UI.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
 
 namespace CustomBinderAndWYSIWYG.Web.UI
 {
-    public class TestTinyMCEModel(IOptions<Keys> keysOptions) : PageModel
-    {
-        private Keys keys = keysOptions.Value;
+	public class TestTinyMCEModel(IOptions<Keys> keysOptions) : PageModel
+	{
+		private Keys keys = keysOptions.Value;
 
-        public void OnGet()
-        {
-            ViewData[nameof(Keys)] = keys;
-        }
-    }
+		public void OnGet()
+		{
+			ViewData[nameof(Keys)] = keys;
+		}
+
+		//public async Task<IActionResult> OnPostUploadImageAsync(IFormFile file)
+		//{
+		//	if (file == null || file.Length == 0)
+		//	{
+		//		return BadRequest("Le fichier est obligatoire.");
+		//	}
+
+		//	// Logique d'upload simplifiée ici...
+
+		//	return new JsonResult(new { location = "/chemin/vers/limage.jpg" });
+		//}
+
+		public async Task<IActionResult> OnPostUploadImageAsync()
+		{
+			var file = Request.Form.Files.GetFile("file"); // "file" est le nom du champ attendu par TinyMCE
+			if (file != null && file.Length > 0)
+			{
+				var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", file.FileName);
+
+				using (var stream = new FileStream(filePath, FileMode.Create))
+				{
+					await file.CopyToAsync(stream);
+				}
+
+				var url = $"{Request.Scheme}://{Request.Host}/images/{file.FileName}";
+				return new JsonResult(new { location = url });
+			}
+
+			return BadRequest("Aucun fichier n'a été uploadé.");
+		}
+	}
 }
--- CustomBinderAndWYSIWYG.Web.UI/Program.cs
+++ CustomBinderAndWYSIWYG.Web.UI/Program.cs
@@ -3,8 +3,20 @@ using CustomBinderAndWYSIWYG.Web.UI.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowSpecificOrigin",
+		builder =>
+		{
+			builder.WithOrigins("https://localhost:7230") // Remplacez par l'URL/port de votre app React
+				   .AllowAnyHeader()
+				   .AllowAnyMethod();
+		});
+});
 
 builder.Services.Configure<Keys>(builder.Configuration.GetSection(nameof(Keys)));
 
@@ -13,18 +25,36 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+
+
+//var fileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.WebRootPath, "images"));
+//var requestPath = "/images";
+app.UseCors("AllowSpecificOrigin");
+//// Enable displaying browser links.
 app.UseStaticFiles();
+//app.UseStaticFiles(new StaticFileOptions
+//{
+//	FileProvider = fileProvider,
+//	RequestPath = requestPath
+//});
+
+//app.UseDirectoryBrowser(new DirectoryBrowserOptions
+//{
+//	FileProvider = fileProvider,
+//	RequestPath = requestPath
+//});
 
 app.UseRouting();
-
 app.UseAuthorization();
 
+app.MapControllers();
 app.MapRazorPages();
 
 app.Run();
